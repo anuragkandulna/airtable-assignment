68914d474aee0abf8fd87d4136af95d1ca071c66
Anurag Kandulna <dev.ak@outlook.in>
2025-08-07 21:34:00 +0530

    airtable read write operations

---
+++ airtable_operations.py
@@ -0,0 +1,54 @@
+import requests
+from utils.config_loader import HEADERS, AIRTABLE_BASE_ID
+
+
+def sanitize_records(records):
+    """
+    Sanitize final applicants records by including only id and fields.
+    """
+    cleaned_records = []
+    for record in records:
+        cleaned_record = {
+            "id": record["id"],
+            "fields": record["fields"]
+        }
+        cleaned_records.append(cleaned_record)
+    return cleaned_records
+
+
+def fetch_records_from_table(table_id):
+    """
+    Fetch all records from a given table.
+    """
+    try:
+        url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{table_id}"
+        response = requests.get(url, headers=HEADERS)
+        data = response.json()
+        return data.get("records", [])
+
+    except Exception as ex:
+        print(f"Error fetching records from {table_id} table: {ex}")
+        return []
+
+
+def upsert_records(table_id, table_name, sanitized_records):
+    """
+    Upsert records to a given table.
+    """
+    url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{table_id}"
+    payload = { "records": sanitized_records }
+
+    try:
+        response = requests.patch(
+            url,
+            headers=HEADERS,
+            json=payload
+        )
+        if response.status_code == 200:
+            print(f"Upserted {len(sanitized_records)} records to {table_name} table successfully.")
+        else:
+            print(f"Failed to upsert to {table_name} table due to error {response.status_code}: {response.text}")
+            raise Exception(f"Failed to upsert to {table_name} table due to error {response.status_code}: {response.text}")
+
+    except Exception as ex:
+        print(f"Failed to upsert to {table_name} table: {ex}")
